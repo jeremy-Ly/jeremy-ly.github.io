dff13319138bf48c1d1ce2950c8fadaa666d2823
jeremy-Ly <jeremy.ly@berkeley.edu>
2023-04-27 21:09:34 -0700

    finished final

---
+++ final/assets/script.js
@@ -0,0 +1,8 @@
+function makeactive(clicked){
+    document.getElementById(clicked).classList.remove("active");
+}
+
+document.onclick = makeactive()
+
+
+document.getElementById("experiencetab").classList.remove("active)
